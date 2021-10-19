3fac3b9f840d2981908db4731e87a88148eba53a
Minhazul Ashim <ashim.ashim7470@gmail.com>
2021-10-19 17:13:16 +0600

    footer section completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Banner from './components/Banner/Banner';
 import BlogsContainer from './components/BlogsContainer/BlogsContainer';
 import Doctors from './components/Doctors/Doctors';
+import Footer from './components/Footer/Footer';
 import Navigation from './components/Navigation/Navigation';
 import Services from './components/Services/Services';
 
@@ -15,6 +16,7 @@ function App() {
         <Services></Services>
         <Doctors></Doctors>
         <BlogsContainer></BlogsContainer>
+        <Footer></Footer>
       </BrowserRouter>
     </div>
   );
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,65 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Footer.css'
+import { BsFacebook, BsTwitter, BsLinkedin } from 'react-icons/bs'
+import { AiOutlineGooglePlus } from 'react-icons/ai'
 
 const Footer = () => {
     return (
-        <div>
-
+        <div className="footer mt-5">
+            <Container>
+                <Row className='pt-5'>
+                    <Col xs={12} md={6}>
+                        <h5>About Medico</h5>
+                        <p>Medico was at the forefront of modern medicine when its founders opened it as a multi-specialty group practice in 1921. In its first century, Medico has introduced many medical firsts, opened facilities around the world and is proud to be ranked among the top hospitals in the country. Now, 100 years later, the vision of the founders remains Cleveland Clinic’s mission: caring for life, researching for health, and educating those who serve.</p>
+                    </Col>
+                    <Col xs={8} md={4}>
+                        <h5>
+                            Address
+                        </h5>
+                        <p>Medico Hospital, Johnson St., Rd-2, New Jersey, USA</p>
+                        <h5>Email</h5>
+                        <p>information@medico.com</p>
+                        <h5>Phone</h5>
+                        <p>+11256486</p>
+                    </Col>
+                    <Col xs={4} md={2}>
+                        <h5>Navigate to</h5>
+                        <ul className='list-unstyled'>
+                            <li><Link className='navlink'>Home</Link></li>
+                            <li><Link className='navlink'>Services</Link></li>
+                            <li><Link className='navlink'>Blogs</Link></li>
+                            <li><Link className='navlink'>Doctors</Link></li>
+                            <li><Link className='navlink'>Contact</Link></li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} className='d-flex flex-column align-items-center'>
+                        <h6>Important Links</h6>
+                        <div className="social-links d-flex justify-content-around w-25 my-3">
+                            <BsFacebook />
+                            <BsTwitter />
+                            <BsLinkedin />
+                            <AiOutlineGooglePlus />
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='mt-3'>
+                    <Col xs={4}>
+                        <p>&copy; Medico | 2021</p>
+                    </Col>
+                    <Col xs={8}>
+                        <ul className="list-unstyled d-flex justify-content-around">
+                            <li>Terms of Condition</li>
+                            <li>Policy</li>
+                            <li>Privacy</li>
+                            <li>Copyrights</li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
